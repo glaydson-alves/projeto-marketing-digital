176e5bc59125092d39ec1ced4c9d1326822a36a3
Glaydson Alves <glaydson.alves36@gmail.com>
2023-08-04 21:03:48 -0300

    iniciado js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// alert("Hello World")
+// console.log("Só quem é dev vai descobir")
+// area de registro
+// VAR
+// LET
+// CONST
+// query=pesquisa, seletor
+let nome = document.querySelector("#nome")
+
+function exibirnome(){
+    console.log(nome.value)
+    if(nome.value = ""){
+        nome.style.border = "2px solid red"
+    }
+    else{
+        nome.style.border = "2px solid green"
+    }
+}
+
+nome.addEventListener("blur",exibirnome)
+// o addEventListener cria um evento para algum elemento, no caso estamos utilizando o evento "blur", que é qaundo o foco sai de um campo.
+
+// nome.addEventListener("focus", function(){
+//     nome.value = "Este campo não deve ficar vazio"
+// })

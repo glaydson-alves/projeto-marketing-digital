7c8a169b9c1f48799cbc19c71e36a1a77f30b39d
Glaydson Alves <glaydson.alves36@gmail.com>
2023-08-07 21:11:27 -0300

    continuação js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ let nome = document.querySelector("#nome")
 
 function exibirnome(){
     console.log(nome.value)
-    if(nome.value = ""){
+    if(nome.value == ""){
         nome.style.border = "2px solid red"
     }
     else{
@@ -23,3 +23,52 @@ nome.addEventListener("blur",exibirnome)
 // nome.addEventListener("focus", function(){
 //     nome.value = "Este campo não deve ficar vazio"
 // })
+
+let idade = document.querySelector("#idade")
+
+idade.addEventListener("blur", function(){
+    if(idade.value >= 18){
+        alert("Você possui 18 anos ou mais!")
+    }
+    else{
+        alert("infelizmente você não é de maior")
+        // essa função irá redirecionar o usuario para um link predeterminado.
+        window.location.href = "https://getbootstrap.com/docs/5.3/components/card/"
+    }
+})
+
+let endereco = document.querySelector("input[name=endereco]")
+
+endereco.addEventListener("blur", function(){
+    console.log(endereco.value.length)
+    if (endereco.value.length >= 35){
+        alert("Texto demasiadamente grande")
+        endereco.style.border = "2px solid red"
+    }
+    else { 
+        alert("endereço OK!")
+        endereco.style.border = "2px solid yellow"
+    }
+})
+
+// keyup vai pegando o que vamos digitamos
+let email = document.querySelector("#email")
+let confirmar = document.querySelector("#confirmar")
+
+let minusculo = email.value.toLowerCase()// tornar o value minusculo
+
+email.addEventListener("keyup", function(){
+    confirmar.value = email.value
+    
+    if(minusculo.indexOf("@") == -1 || minusculo.indexOf(".com") == -1){
+
+    }
+    else{
+
+    }
+
+    console.log(email.value.indexOf("@"))
+    // indexOf indentifica se no value existe algum caractere que esta sendo procurado
+})
+
+// ( || pipe pipe)
